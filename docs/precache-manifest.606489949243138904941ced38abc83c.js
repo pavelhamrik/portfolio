2bcf0f91d2106606fb7f705cbc46a50ee2a38a77
Pavel Hamrik <pavel.hamrik@gmail.com>
2018-12-06 15:33:18 +0100

    build fix

---
+++ docs/precache-manifest.606489949243138904941ced38abc83c.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "9de03392a1e67c19d5086fd4663eee3a",
+    "url": "/static/media/Figure Customer Screen Options.9de03392.png"
+  },
+  {
+    "revision": "1649a402ee803b700ee7",
+    "url": "/static/css/main.b18507d5.chunk.css"
+  },
+  {
+    "revision": "0f6c48b431b86d7debc6746223769d15",
+    "url": "/static/media/Figure Manage Modifiers Detail.0f6c48b4.png"
+  },
+  {
+    "revision": "a18ec2ac30569f71bc2a",
+    "url": "/static/js/1.a18ec2ac.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0fa3029394227aab13c01488592707b5",
+    "url": "/static/media/Figure Register iPad.0fa30293.png"
+  },
+  {
+    "revision": "134e8ca9f33198e34b8f01705e5d63cb",
+    "url": "/static/media/Figure Date Picker.134e8ca9.png"
+  },
+  {
+    "revision": "53dca9342fde43a520a9fd8259aaac8e",
+    "url": "/static/media/Figure Printer Settings.53dca934.png"
+  },
+  {
+    "revision": "87012ec51e5c4e9e8d500321712ad4ce",
+    "url": "/static/media/Figure Payment Flows.87012ec5.png"
+  },
+  {
+    "revision": "f243f94fdb3c94358142e67912630bc9",
+    "url": "/static/media/Figure Custom Item Exclusions.f243f94f.png"
+  },
+  {
+    "revision": "1cc9caf293cc0e02eb43f722d3608d9c",
+    "url": "/static/media/Figure Customer Screen Signature.1cc9caf2.png"
+  },
+  {
+    "revision": "1649a402ee803b700ee7",
+    "url": "/static/js/main.1649a402.chunk.js"
+  },
+  {
+    "revision": "810defb1df7442f848462ddd631c80c6",
+    "url": "/static/media/Figure Cash Register Report.810defb1.png"
+  },
+  {
+    "revision": "1816196cfade83de0e58e87e87bae5f6",
+    "url": "/static/media/Figure Net Revenue Report.1816196c.png"
+  },
+  {
+    "revision": "1658f848afec01c4c9cae72068fc49fe",
+    "url": "/static/media/Figure Payment Summary.1658f848.png"
+  },
+  {
+    "revision": "80268f9cc4c5218b504645e56d7c02f8",
+    "url": "/static/media/Figure Custom Item.80268f9c.png"
+  },
+  {
+    "revision": "66a5813bf7d64143d56df15edc24f2a0",
+    "url": "/static/media/Figure Manage Items and Cats.66a5813b.png"
+  },
+  {
+    "revision": "17e1d1e5cf3bb6a860b793091971ff8b",
+    "url": "/static/media/Figure Manage Items and Cats Detail.17e1d1e5.png"
+  },
+  {
+    "revision": "4206f0e7805832c0e25d7fff1263ae4b",
+    "url": "/static/media/Figure Manage Items and Cats Empty.4206f0e7.png"
+  },
+  {
+    "revision": "82f390eeda0a3c50fcfbf26335677918",
+    "url": "/static/media/Figure Manage Items and Cats Empty Detail.82f390ee.png"
+  },
+  {
+    "revision": "d1168f8bb7e4f3edad08f4387618e012",
+    "url": "/static/media/Figure Manage Modifiers.d1168f8b.png"
+  },
+  {
+    "revision": "a18ec2ac30569f71bc2a",
+    "url": "/static/css/1.9517f78a.chunk.css"
+  },
+  {
+    "revision": "27b5b9e80a78ba6358e74dc6f9909002",
+    "url": "/index.html"
+  }
+];
